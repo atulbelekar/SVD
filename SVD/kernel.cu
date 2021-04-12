a7689544363f41f28082c50ecc0775f4765f368b
Belekar Atul Suresh <183109001@iitb.ac.in>
2021-04-12 22:53:02 +0530

    QR Factorization Completed

--- SVD/kernel.cu
+++ SVD/kernel.cu
@@ -8,11 +8,10 @@
 #include<random>
 using namespace std;
 
-
 __global__ void matmul(Matrix a, Matrix b, Matrix c) {
     
     for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < a.row; i+= (blockDim.x * gridDim.x)) {
-        for (int j = blockIdx.y * blockDim.y + threadIdx.y; j < b.col; j+=(blockDim.x * gridDim.x)) {
+        for (int j = blockIdx.y * blockDim.y + threadIdx.y; j < a.col; j += (blockDim.y * gridDim.x)){
             double sum = 0;
             for (int k = 0; k < a.row; k++) {
                 sum += (a.dp[i * a.col + k] * b.dp[k * b.col + j]);
@@ -24,7 +23,7 @@ __global__ void matmul(Matrix a, Matrix b, Matrix c) {
 
 __global__ void Gram_schmidt(Matrix a, Matrix ans) {
     
-    for (int i = 0; i < a.col; i++) {
+    for (int i =0; i < a.row; i++) {
         for (int j = i - 1; j >= 0; j--) {
             double dot = dot_prod(a, ans, i, j);
 
@@ -45,47 +44,40 @@ __global__ void Gram_schmidt(Matrix a, Matrix ans) {
 
 __global__ void R_matrix(Matrix a, Matrix Q, Matrix R) {
 
-    for (int i = 0; i < a.row; i++) {
-        for (int j = i; j < a.col; j++) {
+    for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < a.row; i += (blockDim.x * gridDim.x)) {
+        for (int j = blockIdx.y * blockDim.y + threadIdx.y; j < a.col; j += (blockDim.y * gridDim.x)) {
             R.dp[i*(R.col)+j] = dot_prod(a, Q, j, i);
         }
     }
 }
 
-
-int main() {
-   
-    Matrix A(2, 2);
-    A.random_init(0, 10);
-    A.cuda_malloc();
-    dim3 size(10, 10);
-    int n = 20;
-    Matrix Q(2, 2);
-    Q.cuda_malloc();
-    Matrix R(2, 2);
+void Find_eigen_values(Matrix &A, Matrix &Q, int n) {
+    Matrix R(A.row, A.col);
     R.cuda_malloc();
+    dim3 size(16, 16);
     while (n--) {
-        cout << "Q : \n";
-        Q.cuda_copy_to_host();
-        Q.print();
-        cout << "R: \n";
-        R.cuda_copy_to_host();
-        R.print();
-        cout << "A: \n";
-        A.cuda_copy_to_host();
-        A.print();
-        cout << "****\n";
-        Q.set_zero();
         Q.cuda_free();
         Q.set_zero();
         Q.cuda_malloc();
-        Gram_schmidt << <1, size >> > (A, Q);
+        Gram_schmidt << <1, 1 >> > (A, Q);
         R_matrix << <1, size >> > (A, Q, R);
         matmul << <1, size >> > (R, Q, A);
-        
     }
-    
+}
 
+int main() {
+   
+    Matrix A(10, 10);
+    A.random_init(5, 50);
+    A.print();
+    cout << "*******\n";
+    A.cuda_malloc();
+    int n = 20;
+    Matrix Q(10, 10);
+    Q.cuda_malloc();
+    Find_eigen_values(A, Q, 100);
+    A.cuda_copy_to_host();
+    A.print();
 
 }
 
